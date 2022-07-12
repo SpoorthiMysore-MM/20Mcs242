84a96dd3d303f3cda39dbd82d40516eefc49066f
SpoorthiMysore-MM <108916749+SpoorthiMysore-MM@users.noreply.github.com>
2022-07-12 12:50:46 +0530

    Add files via upload

---
+++ IndirectMethodInvoke/main/java/com/example/indirectmethodinvocation/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.indirectmethodinvocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+   public void methodIndirect(View view)
+   {
+       Toast.makeText(getApplicationContext(),"Hello Welocme to the MAD LAB... ",Toast.LENGTH_LONG).show();
+
+   }
+
+}

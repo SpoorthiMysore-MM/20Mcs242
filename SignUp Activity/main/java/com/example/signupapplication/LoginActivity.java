f536d9a1ec9383109f3fc6bae8f3c43b7ae74b6b
SpoorthiMysore-MM <108916749+SpoorthiMysore-MM@users.noreply.github.com>
2022-08-02 12:59:40 +0530

    Add files via upload

---
+++ SignUp Activity/main/java/com/example/signupapplication/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.example.signupapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText emailEditText, passwordEditText;
+    Button loginBtn;
+    int counter=2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        emailEditText=findViewById(R.id.emailEditText);
+        passwordEditText=findViewById(R.id.passwordEditText);
+        loginBtn=findViewById(R.id.loginBtn);
+        String registeredEmail=getIntent().getStringExtra("email");
+        String registeredPassword=getIntent().getStringExtra("password");
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=emailEditText.getText().toString();
+                String password=passwordEditText.getText().toString();
+                if(registeredEmail.equals(email)&& registeredPassword.equals(password))
+                {
+                    Intent intent=new Intent(LoginActivity.this,loginSuccessfulActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Invalid Credentials",Toast.LENGTH_LONG).show();
+                }
+                counter--;
+                if (counter==0)
+                {
+                    Toast.makeText(getBaseContext(),"FAILED LOGIN ATTEMPTS",Toast.LENGTH_LONG).show();
+                            loginBtn.setEnabled(false);
+                }
+
+            }
+        });
+
+
+
+    }
+}
